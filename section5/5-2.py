b1410f8d1dce0baad185161ccd0f15acc4f9b378
Masamichi Kanakubo <masamichi.kanakubo@MacBook-Pro-6.local>
2023-12-22 13:57:58 +0900

    docs: create 5 readme

--- section5/5-2.py
+++ section5/5-2.py
@@ -13,17 +13,4 @@ list = [lv1,lv2,lv3,lv4]
 
 print(f'パーティの中で最もレベルの高いメンバのレベルは{max(list)}です。')
 
-"""
-#if文を使った想定される解法
-if lv1 >= lv2 and lv1 >= lv3 and lv1 >= lv4:
-    max = lv1
-elif lv2 >= lv1 and lv2 >= lv3 and lv2 >= lv4:
-    max = lv2
-elif lv3 >= lv1 and lv3 >= lv2 and lv3 >= lv4:
-    max = lv3
-elif lv4 >= lv1 and lv4 >= lv2 and lv4 >= lv3:
-    max = lv4
-
-print(f'パーティの中で最もレベルの高いメンバのレベルは{max}です。')
-"""
-    
+ 
